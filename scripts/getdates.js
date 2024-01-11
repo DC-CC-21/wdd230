b1732d802a31678530c5fe90a5e30d292de9b921
DC-CC-21 <handstands1000@gmail.com>
2024-01-11 14:16:15 -0500

    completed home page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
 let footerP = document.querySelectorAll("footer p")
 let currentDate = new Date()
 
-footerP[0].innerHTML = footerP[0].innerHTML.replace("year", currentDate.getFullYear())
+footerP[0].innerHTML = footerP[0].innerHTML.replace("2024", currentDate.getFullYear())
 footerP[1].innerHTML = document.lastModified
