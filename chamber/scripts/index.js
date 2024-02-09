66325339fe104273584d08b75e529e0e1682cb15
DC-CC-21 <handstands1000@gmail.com>
2024-02-09 18:04:50 -0500

    updated home page

---
+++ chamber/scripts/index.js
@@ -0,0 +1,43 @@
+// Nav ham button
+const nav = document.querySelector("nav");
+function showNav(e) {
+  e.children[0].classList.toggle("on");
+  nav.classList.toggle("open");
+}
+
+let footerP = document.querySelectorAll("footer div:not(#about)>p");
+let currentDate = new Date();
+
+footerP[0].innerHTML = footerP[0].innerHTML.replace(
+  "year",
+  currentDate.getFullYear()
+);
+footerP[
+  footerP.length - 1
+].innerHTML = `Last Modified: ${document.lastModified}`;
+
+const r = document.querySelector(":root");
+let values = [
+  "--%header",
+  "--%nav",
+  "--%nav-wayfind",
+  "--%accent",
+  "--%background",
+  "--%background2",
+  "--%body",
+  ,
+  "--%altTxt",
+];
+function switchMode(e) {
+  e.classList.toggle("on");
+  let rs = getComputedStyle(r);
+  values.forEach((value) => {
+    let v = value;
+    if (e.classList.value.includes("on")) {
+      v = v.replace("%", "d");
+    } else {
+      v = v.replace("%", "l");
+    }
+    r.style.setProperty(value.replace("%", ""), rs.getPropertyValue(v));
+  });
+}
