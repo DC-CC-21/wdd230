458d140b6f1d732999e868131298767813bd12c4
DC-CC-21 <handstands1000@gmail.com>
2024-04-08 17:51:34 -0400

    scoots updated

--- scoots/scripts/index.js
+++ scoots/scripts/index.js
@@ -1,30 +1,20 @@
-String.prototype.toTitleCase = function(){
-    return this.split(' ').map(function(word){
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    }).join(' ');
-};
-
-
 // > SLIDESHOW
 let current = 0;
 let images = document.querySelectorAll("#heroImages img");
 
-let height = images[0].clientHeight; 
+function slideshow() {
+  images[current].classList.add("off");
+  current++;
+  if (current >= images.length) {
+    current = 0;
+  }
+  images[current].classList.remove("off");
 
-
-
-
-
-function slideshow(){
-    images[current].classList.add("off")
-    current ++;
-    if(current >= images.length){
-        current = 0;
-    }
-    images[current].classList.remove("off")
-
-
-    setTimeout(slideshow, 5000);
+  setTimeout(slideshow, 5000);
 }
 
 slideshow();
+
+document.querySelector(".banner button").addEventListener("click", (_) => {
+  document.querySelector(".banner").style.display = "none";
+});
